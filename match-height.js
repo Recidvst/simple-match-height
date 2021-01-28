347f6c3a6b5fe6ffc20faefb654f81957a3991b7
Chris Snowden <cdsnowden@gmail.com>
2021-01-28 10:30:08 +0000

    Update formatting

--- match-height.js
+++ match-height.js
@@ -1,21 +1,22 @@
 export function matchSomeHeights(array) {
 	if ( !array || !Array.isArray(array) ) return false; 
-    setTimeout( (time) => { // allow for paint
-        let heights = [];
-        array.forEach( (top) => {
-						// remove current height to allow recalculation
-						if (top.style.removeProperty) {
-							top.style.removeProperty('height');
-						} else {
-							top.style.removeAttribute('height');
-						}
-            const insideHeight = window.getComputedStyle(top, null).getPropertyValue('height');
-            heights.push(parseInt(insideHeight));
-        });
-        // find max height
-        const biggest = Math.max(...heights);
-        array.forEach( (top) => {
-            top.style.height = biggest + 'px';
-        });
-    }, 250);
+  setTimeout( () => {
+    const heights = [];
+    array.forEach( (top) => {
+      // remove current height to allow recalculation
+      if (top.style.removeProperty) {
+        top.style.removeProperty('height');
+      }
+      else {
+        top.style.removeAttribute('height');
+      }
+      const insideHeight = window.getComputedStyle(top, null).getPropertyValue('height');
+      heights.push(parseInt(insideHeight, 10));
+    });
+    const biggest = Math.max(...heights);
+    array.forEach( (top) => {
+      top.style.height = `${biggest}px`;
+    });
+		return true;
+  }, 250);
 }
